dfbaa2d2c42462c8e490e5bf89b57a37545d53c6
surendraLongre <murari.longre2366@gmail.com>
2023-07-24 07:30:29 +0530

    add custom widget which includes other widgets and return it

--- diceefl/lib/main.dart
+++ diceefl/lib/main.dart
@@ -11,31 +11,40 @@ class DicePage extends StatelessWidget {
 		Widget build(BuildContext context) {
 			return MaterialApp(
 					home: Scaffold(
-//						backgroundColor:Colors.red,
+						//						backgroundColor:Colors.red,
 						appBar: AppBar(
 							title: Text('Dicee'),
-//							backgroundColor:Colors.red,
+							//							backgroundColor:Colors.red,
 							),
-						body:
-						Container(
-							decoration:BoxDecoration(
-								gradient:LinearGradient(
-									colors: [
-									Colors.blue.shade100,
-									Colors.blue.shade900,
-									],
-									),
+						body: GradientContainer(),
+						),
+					);
+		}
+}
+
+class GradientContainer extends StatelessWidget{
+	@override
+		Widget build(BuildContext context){
+			return Container(
+					decoration:BoxDecoration(
+						gradient:LinearGradient(
+							colors: [
+							Colors.blue.shade500,
+							Colors.blue.shade900,
+							],
+							begin:Alignment.topLeft,
+							end:Alignment.bottomRight,
+							),
+						),
+					child: Center(
+						child: Text('hello world',
+							style:TextStyle(
+								color:Colors.white,
+								fontSize:28,
 								),
-							child: Center(
-								child: Text('hello world',
-									style:TextStyle(
-										color:Colors.white,
-										fontSize:28,
-									),
-									),
-							      ),
 							),
-				),
-				);
+						),
+					);
+
 		}
 }
